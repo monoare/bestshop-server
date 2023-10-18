f1c25281d8c014b25fc3ea58739a457889ffbf36
ProgrammingHero1 <mamun2k9kuet@gmail.com>
2023-10-19 00:41:22 +0600

    set up the dotenv for dynamically access database

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-//  MONGODB DATABASE USER PASSWORD
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.qfcfzds.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qfcfzds.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,6 +23,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const bestShop = client.db("BestShopDB").collection("Brand");
+
+    app.post("/brands", async (req, res) => {
+      const brandName = req.body;
+      console.log(brandName);
+      const result = await bestShop.insertOne(brandName);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
